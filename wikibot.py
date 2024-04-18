92294510c2351b913644c2710143209ba90d608c
Data-Science-Jagannath <157683814+Data-Science-Jagannath@users.noreply.github.com>
2024-04-18 05:19:05 +0000

    adding test for cli

--- wikibot.py
+++ wikibot.py
@@ -1,7 +1,13 @@
-import wikipedia
+from mylib.bot import scrape
+import click
 
-def scrape(name="Microsoft",length=1):
-    results = wikipedia.summary(name,sentences=length)
-    return results
+@click.command()
+@click.option('--name', prompt='Wikipedia page to scrape',
+              help='web page we want to scrape')
 
-print(scrape("Facebook"))
+def cli(name):
+    results = scrape(name)
+    click.echo(click.style(f"{results}",bg='red', fg="blue"))
+
+if __name__ == '__main__':
+    cli()
